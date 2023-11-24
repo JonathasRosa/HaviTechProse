074c8f3867fdc9eabb19e4ca6b18446a3554c3fd
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-11-24 11:25:03 -0300

    Feat: Structuring the home

--- miniblog/src/pages/Home/Home.js
+++ miniblog/src/pages/Home/Home.js
@@ -1,11 +1,34 @@
-import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
 //CSS
 import styles from './Home.module.css';
 
 const Home = ()=> {
+    const [query, setQuery] = useState("");
+    const [post] = useState([]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={styles.home}>
+            <h1>Veja os nossos posts mais recentes.</h1>
+            <form onSubmit={handleSubmit} className={styles.search_form}>
+                <input 
+                    type="text" 
+                    placeholder="Ou busque por tags"
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button className=" btn btn-dark">Pesquisar</button>
+            </form>
+            <div>
+                <h1>Post</h1>
+                {post && post.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>Não foram encontrados posts</p>
+                        <Link to="/post/create" className="btn">Criar primeiro post.</Link>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
